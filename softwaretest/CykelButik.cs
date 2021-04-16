019e40f5c6c886e7d42403bb17c30c0c2e38df16
Mathias Holst <holsthategaming@gmail.com>
2021-04-16 09:25:31 +0200

    + Cykelbutik med Tests og UML diagram

---
+++ softwaretest/CykelButik.cs
@@ -0,0 +1,53 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace softwaretest
+{
+    public class Cykel
+    {
+        public string navn;
+        public string farve;
+        public int antal_hjul;
+        public double pris;
+
+        public Cykel(string navn, string farve, int antal_hjul, double pris)
+        {
+            this.navn = navn;
+            this.farve = farve;
+            this.antal_hjul = antal_hjul;
+            this.pris = pris;
+        }
+
+        public void opdaterCykelFarve(string farve)
+        {
+            this.farve = farve;
+        }
+
+    }
+    public class CykelButik
+    {
+        public string navn;
+        public List<Cykel> listeAfCykler = new List<Cykel>();
+
+        public void tilføjCykel(Cykel cykel)
+        {
+            listeAfCykler.Add(cykel);
+        }
+        public Cykel[] hentCykler()
+        {
+            Cykel[] testCykelArray = new Cykel[listeAfCykler.Count];
+
+            for(int i = 0; i < testCykelArray.Length; i++)
+            {
+                testCykelArray[i] = listeAfCykler[i];
+            }
+            return testCykelArray;
+        }
+
+        public void fjernCykel(Cykel cykel)
+        {
+            listeAfCykler.Remove(cykel);
+        }
+    }
+}
---
+++ softwaretestTests/CykelButikTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using softwaretest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace softwaretest.Tests
+{
+    [TestFixture()]
+    public class CykelButikTests
+    {
+        [Test]
+        public void CykelConstructorSætterVariabler()
+        {
+            var nyCykel = new Cykel("RacerCykel", "blå", 2, 11000.00);
+            Assert.That(nyCykel.navn == "RacerCykel");
+            Assert.That(nyCykel.farve == "blå");
+            Assert.That(nyCykel.antal_hjul == 2);
+            Assert.That(nyCykel.pris == 11000.00);
+        }
+        [Test]
+        public void NyCykelFarveTest()
+        {
+            var nyCykel = new Cykel("Dame Cykel", "sårt", 2, 3999.95);
+            nyCykel.opdaterCykelFarve("sort");
+            Assert.That(nyCykel.farve == "sort");
+        }
+
+        [Test]
+        public void TilføjCykelTest()
+        {
+            var nyCykel = new Cykel("Trehjulet Cykel", "grøn", 3, 1499.95);
+            var nyCykelButik = new CykelButik();
+            nyCykelButik.tilføjCykel(nyCykel);
+            Assert.That(nyCykelButik.listeAfCykler[0] == nyCykel);
+        }
+
+        [Test]
+        public void HentCyklerTest()
+        {
+            var nyCykelButik = new CykelButik();
+            var nyCykel = new Cykel("Et hjulet Cykel", "sølv", 1, 999.95);
+            nyCykelButik.tilføjCykel(nyCykel);
+            int antalCyklerIListe = nyCykelButik.hentCykler().Length;
+            Assert.That(nyCykelButik.hentCykler().Length == antalCyklerIListe);
+        }
+
+        [Test]
+        public void FjernCykelTest()
+        {
+            var nyCykel = new Cykel("Væltepeter", "grøn", 2, 3999.95);
+            var nyCykelButik = new CykelButik();
+            nyCykelButik.tilføjCykel(nyCykel);
+            Assert.That(nyCykelButik.listeAfCykler.Count == 1);
+            nyCykelButik.fjernCykel(nyCykel);
+            Assert.That(nyCykelButik.listeAfCykler.Count == 0);
+        }
+    }
+}
